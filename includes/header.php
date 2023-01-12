76f0c01fe3518b0d7c02ec49f3cc3246c325b087
Carlos Jesús Morales pica <carlosjesus.moralespica2000@gmail.com>
2023-01-12 13:44:35 +0100

    Empezando el formulario

---
+++ includes/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP CON CRUD</title>
+    <link rel="stylesheet" href="app/css/app.css" />
+    <script defer src="app.js"></script>
+</head>
+<body>
+    <header class="header">
+        <nav class="header__nav">
+            <a href="index.php" class="header__link">PHP MYSQL CRUD</a>
+        </nav>
+    </header>
--- index.php
+++ index.php
@@ -1,15 +1,21 @@
 <?php include("db.php");?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP CON CRUD</title>
-    <link rel="stylesheet" href="app/css/app.css" />
-</head>
-<body>
-    
-</body>
-</html>
+<?php include("includes/header.php");?>
+
+    <main class="main">
+        <section class="main__section">
+            <div class="main__container">
+                <form action="save_task.php" class="main__form" method="POST">
+                    <div class="main__control">
+                        <input type="text" name="title" class="main__input" placeholder="Task Tittle" autofocus>
+                    </div>
+                    <div class="main__group">
+                        <textarea name="description" cols="30" rows="10" class="main__textarea" placeholder="Task description"></textarea>
+                    </div>
+                    <input type="submit" value="Save Task" name="save_task" class="main__submit">
+                </form>
+            </div>
+        </section>
+    </main>
+
+<?php include("includes/footer.php")?>
